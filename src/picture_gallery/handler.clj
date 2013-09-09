a87e9d7423ce927278db2bc9155d61714a22d7fc
dmitryame <dmitry@custommode.com>
2013-09-08 22:16:41 -0400

    adding file upload

--- src/picture_gallery/handler.clj
+++ src/picture_gallery/handler.clj
@@ -7,8 +7,9 @@
 ;            [compojure.route :as route]
 ;            [picture-gallery.routes.home :refer [home-routes]]
 ;            [noir.util.middleware :as noir-middleware]))
-(ns picture-gallery.handler(:require [compojure.route :as route]            [compojure.core :refer [defroutes]]            [noir.util.middleware :as noir-middleware]            [picture-gallery.routes.home :refer [home-routes]]
-            [picture-gallery.routes.auth :refer [auth-routes]]))
+(ns picture-gallery.handler(:require [compojure.route :as route]          [compojure.core :refer [defroutes]]          [noir.util.middleware :as noir-middleware]          [picture-gallery.routes.home :refer [home-routes]]
+          [picture-gallery.routes.auth :refer [auth-routes]]
+          [picture-gallery.routes.upload :refer [upload-routes]] ))
 
 (defn init []
   (println "picture-gallery is starting"))
@@ -20,17 +21,11 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
-;(def app (handler/site (routes home-routes app-routes)))
-(def app (noir-middleware/app-handler [home-routes app-routes]))
-
-;(def war-handler 
-;  (-> app    
-;    (wrap-resource "public") 
-;    (wrap-base-url)
-;    (wrap-file-info)))
- (def war-handler (noir-middleware/war-handler app)) 
 
 (def app (noir-middleware/app-handler 
            [auth-routes
             home-routes
+            upload-routes
             app-routes]))
+
+(def war-handler (noir-middleware/war-handler app)) 
