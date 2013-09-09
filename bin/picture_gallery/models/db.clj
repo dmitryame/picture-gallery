73b197d1f7f7f4752dcbee23a4d16620d090a577
dmitryame <dmitry@custommode.com>
2013-09-08 21:23:32 -0400

    removing bin resources from gitignore

--- bin/picture_gallery/models/db.clj
+++
@@ -1,10 +0,0 @@
-(ns picture-gallery.models.db
-   (:require [clojure.java.jdbc :as sql]))
-
-(def db
-  {:subprotocol "postgresql"
-   :subname "//localhost/gallery" 
-   :user "admin"
-   :password "admin"})
-
-(defn create-user [user] (sql/with-connection    db    (sql/insert-record :users user)))
